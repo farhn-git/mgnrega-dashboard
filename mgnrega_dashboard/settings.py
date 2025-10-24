6ee6239d389de13a63cca69786fc9dfbde2860f9
farhn-git <m.farhaann.p@gmail.com>
2025-10-24 12:28:19 +0530

    debugging

--- mgnrega_dashboard/settings.py
+++ mgnrega_dashboard/settings.py
@@ -122,17 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 # settings.py
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'dashboard' / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'dashboard' / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
