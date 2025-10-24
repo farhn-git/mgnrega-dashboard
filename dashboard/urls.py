d4ff9de3ece50da8d7452e82c8117d6457fc7c51
farhn-git <m.farhaann.p@gmail.com>
2025-10-24 23:37:24 +0530

    final cleanup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('district/', views.district_view, name='district'),
-    path('load-records/', views.load_records),
-    path('sync-data/', views.sync_data),
 ]
