09840ffc2fb0100fee10afc95741fe61b4564ebb
farhn-git <m.farhaann.p@gmail.com>
2025-10-24 11:21:34 +0530

    fixin error 505

--- mgnrega_dashboard/settings.py
+++ mgnrega_dashboard/settings.py
@@ -76,9 +76,19 @@ WSGI_APPLICATION = 'mgnrega_dashboard.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+import dj_database_url
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
